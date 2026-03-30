cf60da2d98ded1e306c0f57e6bc5ff532660d9f2
XERCORD <xerly.ji@ynov.com>
2026-03-30 14:40:27 +0200

    remove hardcoded database password for security

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'xerly2058',
+  password: '',
   database: 'e_commerce_js'
 });
 
