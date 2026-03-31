01da5ec7ee469a636c0a70ee0cd60eb944c74dbf
XERCORD <xerly.ji@ynov.com>
2026-04-01 00:13:53 +0200

    amélioration

--- backend/app.js
+++ backend/app.js
@@ -8,62 +8,65 @@ app.use(cors());
 const productsRoutes = require('./routes/products');
 app.use(express.json());
 
-// Servir les fichiers statiques (CSS, JS, images)
+// Servir les fichiers statiques (CSS, JS, images) depuis le dossier assets
 app.use('/assets', express.static(path.join(__dirname, '../frontend/assets')));
+
+// Servir les fichiers HTML comme fichiers statiques
+app.use(express.static(path.join(__dirname, '../frontend/templates')));
 
 // Routes API
 app.use('/api', productsRoutes);
 
-// Routes pour chaque page HTML
+// Routes pour chaque page HTML (optionnel si vous utilisez les fichiers statiques)
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/templates/index.html'));
-});
-
-app.get('/allproducts', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/templates/allproducts.html'));
 });
 
-app.get('/article', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/templates/article.html'));
-});
-
-app.get('/panier', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/templates/panier.html'));
-});
-
-app.get('/favorite', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/templates/favorite.html'));
-});
-
-app.get('/promotions', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/templates/promotions.html'));
-});
-
-app.get('/recherche', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/templates/recherche.html'));
-});
-
-app.get('/licence', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/templates/licence.html'));
-});
-
-app.get('/categorie', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/templates/categorie.html'));
-});
-
-app.get('/contact', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/templates/contact.html'));
-});
-
-app.get('/about', (req, res) => {
+app.get('/about.html', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/templates/about.html'));
 });
 
-app.get('/paiment', (req, res) => {
+app.get('/contact.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/templates/contact.html'));
+});
+
+app.get('/allproducts.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/templates/allproducts.html'));
+});
+
+app.get('/article.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/templates/article.html'));
+});
+
+app.get('/panier.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/templates/panier.html'));
+});
+
+app.get('/favorite.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/templates/favorite.html'));
+});
+
+app.get('/promotions.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/templates/promotions.html'));
+});
+
+app.get('/recherche.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/templates/recherche.html'));
+});
+
+app.get('/licence.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/templates/licence.html'));
+});
+
+app.get('/categorie.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/templates/categorie.html'));
+});
+
+app.get('/paiment.html', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/templates/paiment.html'));
 });
 
-app.get('/confirmation', (req, res) => {
+app.get('/confirmation.html', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/templates/confirmation.html'));
 });
 
